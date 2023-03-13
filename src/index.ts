e9f03e1a07f995900cb12cba51dc37678d2c3ee5
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-13 17:10:27 +0200

    feat: fixed config bug

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
+
+app.get('/', (req, res) => {
+	return res.json({
+		status: 200,
+		message: 'Welcome To Phantom Server.',
+		data: []
+	})
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => { console.log(`Server is running at http://localhost:${port}`) })
