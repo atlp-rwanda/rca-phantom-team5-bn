706c2c58d27c3204456ee9244a35230e77963b05
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2023-03-21 15:05:26 +0200

    reducing coginative complex

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -13,26 +13,34 @@ export const authorize = (allowedAccessTypes: string[]) => async (req: Request, 
   try {
     let jwt = req.headers.authorization;
 
-    // verify request has token
+ 
     if (!jwt) {
       return ResponseUtil.handleError(FORBIDDEN, 'Invalid token ');
     }
 
-    // remove Bearer if using Bearer Authorization mechanism
     if (jwt.toLowerCase().startsWith('bearer')) {
       jwt = jwt.slice('bearer'.length).trim();
     }
 
-    // verify token hasn't expired yet
+  
+    const isRevoked = await (async () => {
+      try {
+        const value = await client.get(jwt as string);
+        if (value === 'revoked') return false;
+        return true;
+      } catch (error) {
+        return false;
+      }
+    })();
 
-    const isRevoked = await checkRevoked(jwt as string);
 
     if (isRevoked) return ResponseUtil.handleError(FORBIDDEN, 'Your token is not valid');
 
 
     const decodedToken = await validateToken(jwt as string);
-
-    const hasAccessToEndpoint = hasAcess(allowedAccessTypes, decodedToken);
+    const  hasAccessToEndpoint = allowedAccessTypes.length === 0 || decodedToken.accessTypes.some(accessType => allowedAccessTypes.includes(accessType));
+    
+  
 
     if (!hasAccessToEndpoint) {
       return ResponseUtil.handleError(FORBIDDEN, 'You are not authorized to access this endpoint');
@@ -43,20 +51,3 @@ export const authorize = (allowedAccessTypes: string[]) => async (req: Request, 
     return ResponseUtil.handleError(INTERNAL_SERVER_ERROR, 'Failed to authenticate user');
   }
 };
-
-const checkRevoked = async (jwt: string) => {
-  try {
-    const value = await client.get(jwt);
-    if (value === 'revoked') return false;
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
-
-const hasAcess = (allowedAccessTypes: string[], decodedToken: any) => {
-  const hasAccessToEndpoint = allowedAccessTypes.length < 1 ? true : allowedAccessTypes.some(
-    (at) => decodedToken.accessTypes.some((uat: string) => uat === at)
-  );
-  return hasAccessToEndpoint;
-}
