3362c9905650ea6beb0733af5e4e95887acf4ddc
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2023-03-21 15:34:50 +0200

    refactoring

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,43 +4,43 @@ import client from '../utils/connectRedisUtils';
 import ResponseUtil from '../utils/responseUtil'
 import { FORBIDDEN, INTERNAL_SERVER_ERROR } from 'http-status';
 
-
-
-const isAuthorized = (allowedAccessTypes: string[], decodedToken: any) => {
-  return allowedAccessTypes.length === 0 || decodedToken.accessTypes.some((accessType: string) => allowedAccessTypes.includes(accessType));
+const isTokenValid = async (jwt: string) => {
+  try {
+    const value = await client.get(jwt);
+    return value !== 'revoked';
+  } catch (error) {
+    return false;
+  }
 }
 
-/**
- * middleware to check whether user has access to a specific endpoint
- *
- * @param allowedAccessTypes list of allowed access types of a specific endpoint
- */
+const isAuthorized = (allowedAccessTypes: string[], decodedToken: any) => {
+  if (allowedAccessTypes.length === 0) {
+    return true;
+  }
+  for (const accessType of decodedToken.accessTypes) {
+    if (allowedAccessTypes.includes(accessType)) {
+      return true;
+    }
+  }
 
-export const authorize = (allowedAccessTypes: string[]=[]) => async (req: Request, res: Response, next: NextFunction) => {
+  return false;
+};
+
+
+export const authorize = (allowedAccessTypes: string[]) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     const jwt = req.headers.authorization?.replace('Bearer ', '');
-    if (!jwt) {
-      return ResponseUtil.handleError(FORBIDDEN, 'Invalid token');
-    }
+    if (!jwt){return ResponseUtil.handleError(FORBIDDEN, 'Invalid token');}
 
     const isValid = await isTokenValid(jwt);
-    if (!isValid) {
-      return ResponseUtil.handleError(FORBIDDEN, 'Your token is not valid');
-    }
+    if (!isValid){return ResponseUtil.handleError(FORBIDDEN, 'Your token is not valid')};
 
     const decodedToken = await validateToken(jwt);
-    const hasAccessToEndpoint = isAuthorized(allowedAccessTypes, decodedToken);
-    if (!hasAccessToEndpoint) {
-      return ResponseUtil.handleError(FORBIDDEN, 'You are not authorized to access this endpoint');
-    }
+    const hasAccess = isAuthorized(allowedAccessTypes, decodedToken);
+    if (!hasAccess) {return ResponseUtil.handleError(FORBIDDEN, 'You are not authorized to access this endpoint')};
 
     next();
-  } catch (error: any) {
+  } catch {
     return ResponseUtil.handleError(INTERNAL_SERVER_ERROR, 'Failed to authenticate user');
   }
 };
-
-const isTokenValid = async (jwt: string) => {
-  const value = await client.get(jwt);
-  return value === 'revoked' ? false : true;
-}
