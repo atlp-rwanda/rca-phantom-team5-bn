1de0dc500d140ce27b8b5178231170449dc89512
yassin <hyassin509@gmail.com>
2023-04-01 22:14:14 +0200

    fix tests

--- src/database/configs/config.ts
+++ src/database/configs/config.ts
@@ -1,23 +1,45 @@
 import dotenv from 'dotenv'
-
-
 dotenv.config()
 module.exports = {
     test: {
-        dialect: 'postgres',
-        url: process.env.DATABASE_URL,
+        // logging: false,
+        // dialect: 'postgres',
+        // url: process.env.DATABASE_URL_TEST,
+
         logging: false,
+        dialect: 'postgres',
+        port: process.env.DATABASE_PORT,
+        host:  process.env.DATABASE_HOST,
+        database:  process.env.DATABASE_NAME,
+        username:  process.env.DATABASE_USERNAME,
+        password:  process.env.DATABASE_PASSWORD,
     },
 
     development: {
-        dialect: 'postgres',
-        url: process.env.DATABASE_URL_TEST,
+        // logging: false,
+        // dialect: 'postgres',
+        // url: process.env.DATABASE_URL,
+
         logging: false,
+        dialect: 'postgres',
+        port: process.env.DATABASE_PORT,
+        host:  process.env.DATABASE_HOST,
+        database:  process.env.DATABASE_NAME,
+        username:  process.env.DATABASE_USERNAME,
+        password:  process.env.DATABASE_PASSWORD,
     },
 
     production: {
-        dialect: 'postgres',
-        url: process.env.DATABASE_URL,
+        // logging: false,
+        // dialect: 'postgres',
+        // url: process.env.DATABASE_URL,
+
         logging: false,
+        dialect: 'postgres',
+        port: process.env.DATABASE_PORT,
+        host:  process.env.DATABASE_HOST,
+        database:  process.env.DATABASE_NAME,
+        username:  process.env.DATABASE_USERNAME,
+        password:  process.env.DATABASE_PASSWORD,
     }
 }
