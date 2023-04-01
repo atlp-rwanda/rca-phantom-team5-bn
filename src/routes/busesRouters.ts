8a32c52daf2a7132d38c1a4d78bfa456e693e520
yassin <hyassin509@gmail.com>
2023-04-01 12:13:12 +0200

    protected bus routes

--- src/routes/busesRouters.ts
+++ src/routes/busesRouters.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { authorizationToken } from '../middlewares/authMiddleware'
+import { authorizationToken,userAuthorization } from '../middlewares/authMiddleware'
 import busesController from "../modules/buses/controller/busesController"
 import { validateCreateBus,validateUpdateBus } from '../modules/buses/validator/busValidator'
 
@@ -8,9 +8,9 @@ const busRouter=Router()
 
 busRouter.get("/get-buses", busesController.getBuses)
 busRouter.get("/get-bus/:id", busesController.getBus)
-busRouter.post("/create-bus", authorizationToken, validateCreateBus , busesController.createBus)
-busRouter.delete("/delete-bus/:id", authorizationToken, busesController.deleteBus)
-busRouter.put("/update-bus/:id", authorizationToken , validateUpdateBus , busesController.updateBus)
+busRouter.post("/create-bus", authorizationToken,userAuthorization(['operator']), validateCreateBus , busesController.createBus)
+busRouter.delete("/delete-bus/:id", authorizationToken , userAuthorization(['operator']) , busesController.deleteBus)
+busRouter.put("/update-bus/:id", authorizationToken ,userAuthorization(['operator']) , validateUpdateBus , busesController.updateBus)
 
 
 export default busRouter
