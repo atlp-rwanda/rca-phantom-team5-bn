5326a3c93d81394bbd7596bca95f27cbf4386a62
yassin <hyassin509@gmail.com>
2023-04-02 13:00:38 +0200

    swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', routes)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 app.get('**', (req: Request, res: Response) => res.status(200).json({
