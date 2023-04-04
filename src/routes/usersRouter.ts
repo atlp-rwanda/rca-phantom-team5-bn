cba599aa2939b7e76f8d9d363d66d2e75a2461a1
Gahamanyi-dev <ygahamanyi26@gmail.com>
2023-04-04 11:35:11 +0200

    fix: changed the middleware

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { authorizationToken } from '../middlewares/middleware'
+import { userAuthorization } from '../middlewares/middleware'
 import usersController from '../modules/users/controller/usersController'
 import { validateUpdateUser } from '../modules/users/validator/userValidator'
 
@@ -7,7 +7,7 @@ const usersRouter = Router()
 
 usersRouter
 	.get('/get-users', usersController.getUsers)
-	.get('/get-profile', authorizationToken, usersController.getProfile)
-	.put('/update-profile', authorizationToken, validateUpdateUser, usersController.updateProfile)
+	.get('/get-profile', userAuthorization(['operator', 'driver', 'adamin', 'super_admin']), usersController.getProfile)
+	.put('/update-profile', userAuthorization(['operator', 'driver', 'adamin', 'super_admin']), validateUpdateUser, usersController.updateProfile)
 
 export default usersRouter
