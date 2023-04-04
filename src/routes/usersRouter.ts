0464b36d775ae56f7eaac728831fd916ffef15d4
Gahamanyi-dev <ygahamanyi26@gmail.com>
2023-04-04 09:12:57 +0200

    fix: get profile and update profile error

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -2,14 +2,11 @@ import { Router } from 'express'
 import { authorizationToken } from '../middlewares/middleware'
 import usersController from '../modules/users/controller/usersController'
 import { validateUpdateUser } from '../modules/users/validator/userValidator'
-import { authorizationToken } from '../middlewares/authMiddleware'
 
 const usersRouter = Router()
 
 usersRouter
 	.get('/get-users', usersController.getUsers)
-	.put('/update-profile/:id', validateUpdateUser, usersController.updateProfile)
-	.put('/update-profile/:id', authorizationToken, validateUpdateUser, usersController.updateProfile)
 	.get('/get-profile', authorizationToken, usersController.getProfile)
 	.put('/update-profile', authorizationToken, validateUpdateUser, usersController.updateProfile)
 
