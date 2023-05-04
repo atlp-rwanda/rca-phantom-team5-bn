4dc80f6d3f9f9cf3c83e72213e13322c988e7930
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2023-05-04 10:56:27 +0200

    handling error

--- src/modules/socket/index.ts
+++ src/modules/socket/index.ts
@@ -4,12 +4,15 @@ import onJoin from "./onJoin";
 import onUpdate from "./onUpdate";
 import onDisconnect from "./onDisconnect";
 
-export default function  SocketConnection(socket:Socket,io:Server){
+export default function SocketConnection(socket: Socket, io: Server) {
+      try {
+            socket.on('join', (data, callback) => onJoin(socket, data, callback, io));
 
-      socket.on('join',(data,callback)=>onJoin(socket,data,callback,io));
-    
-      socket.on('update', (data,callBack)=> onUpdate(data,callBack,io));
-    
-      socket.on('disconnect', (key) => onDisconnect(key,io))
- 
+            socket.on('update', (data, callBack) => onUpdate(data, callBack, io));
+
+            socket.on('disconnect', (key) => onDisconnect(key, io))
+      } catch (err:any) {
+            console.log(err.message);
+      }
+
 }
