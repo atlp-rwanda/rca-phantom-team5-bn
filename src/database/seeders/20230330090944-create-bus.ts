af7102a47fe358fad3925f5b74e32a6e7ce4c717
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2023-05-04 09:05:45 +0200

    fixing error

--- src/database/seeders/20230330090944-create-bus.ts
+++ src/database/seeders/20230330090944-create-bus.ts
@@ -1,4 +1,4 @@
-import { QueryInterface  } from 'sequelize'
+import { QueryInterface } from 'sequelize'
 
 const busOne = {
   createdAt: new Date(),
@@ -13,8 +13,19 @@ const busOne = {
 const busTwo = {
   createdAt: new Date(),
   updatedAt: new Date(),
-  name: 'BMW',
+  name: 'KBS',
   available_sits: 5,
+  route_id: 2,
+  model: 'XLiu',
+  plate_number: 'RCA125G'
+
+}
+const busThree = {
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: 'Kbs',
+  available_sits: 5,
+  route_id: 2,
   model: 'XLiu',
   plate_number: 'RCA125A'
 
@@ -24,7 +35,7 @@ const busFour = {
   createdAt: new Date(),
   updatedAt: new Date(),
   name: 'Safaris',
-  route_id:4,
+  route_id: 4,
   available_sits: 5,
   model: 'benz',
   plate_number: 'RCA125F'
@@ -34,12 +45,12 @@ const busFive = {
   createdAt: new Date(),
   updatedAt: new Date(),
   name: 'Volcano',
-  route_id:3,
+  route_id: 3,
   available_sits: 5,
   model: 'Honda',
   plate_number: 'RCA125C'
 
 }
-const up = (queryInterface: QueryInterface) => queryInterface.bulkInsert('buses', [busOne, busTwo,busThree,busFour,busFive,busThree,busFour,busFive])
+const up = (queryInterface: QueryInterface) => queryInterface.bulkInsert('buses', [busOne, busTwo, busThree, busFour, busFive])
 const down = (queryInterface: QueryInterface) => queryInterface.bulkDelete('buses', [], {})
 export { up, down }
